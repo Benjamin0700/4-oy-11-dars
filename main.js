2896486174683c2fa0aee62cbc63bcdac26be782
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2024-08-04 00:37:52 +0500

    4 oy 11 dars

---
+++ main.js
@@ -0,0 +1,91 @@
+// 1
+// // const son1 = +prompt(`Birinchi sonni kiriting`);
+// // const son2 = +prompt(`Ikkinchi sonni kiriting`);
+
+// // const amal = +prompt(`
+// // 1. Qo'shish`);
+
+// // let natija;
+
+// // if (amal === 1) {
+// //     natija = son1 + son2;
+// //     console.log(`${son1} + ${son2} = ${natija}`);
+// // } else {
+// //     console.log("Noto'g'ri tanlov!");
+// // }
+
+
+
+
+// 2
+// // let a = '12'; 
+// // let b = Number(a); 
+// // console.log(b); 
+
+
+
+
+// 4
+// const random = (Math.round(Math.random() * 100))
+// alert(`Random son: ${random}`)
+// if (random % 2 == 0 ) {
+//     alert(`${random} juft `);
+// } else {
+//     alert(`${random} toq `);
+// }
+
+
+
+
+// 5
+// let words = ['apple', 'banana', 'grape', 'strawberry', 'kiwi'];
+
+// let longWords = [];
+
+// for (let i = 0; i < words.length; i++) {
+//     if (words[i].length >= 5) {
+//         longWords.push(words[i]);
+//     }
+// }
+
+// console.log(longWords); 
+
+
+
+
+// 6
+// let juft = [];
+// let toq = [];
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+
+//         juft.push(i);
+//     } else {
+
+//         toq.push(i);
+//     }
+// }
+
+// console.log("Juft sonlar:", juft);
+// console.log("Toq sonlar:", toq);
+
+
+
+// 7
+// let arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// let ism = "jaloliddin";
+
+// let javob = "";
+
+// for (let i = 0; i < ism.length; i++) {
+//     let char = ism[i];
+//     let index = arr.indexOf(char);
+
+//     if (index !== -1) {
+//         javob += arr[index];
+//     }
+// }
+
+// console.log(javob);
